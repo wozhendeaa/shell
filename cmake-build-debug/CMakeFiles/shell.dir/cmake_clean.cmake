837e27786d2ca86d97d4100f73dab5c3e5c0f003
wozhendeaa <billyyang520@gmail.com>
2019-10-20 07:11:28 -0400

    IO redirection

---
+++ cmake-build-debug/CMakeFiles/shell.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shell.dir/shell.c.o"
+  "shell"
+  "shell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/shell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
